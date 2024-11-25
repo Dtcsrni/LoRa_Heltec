275dc2b79006ad74dc199c3ddf61b055d44aca0e
Erick Vega <armsystechno@gmail.com>
2024-11-25 15:32:48 -0600

    Update nodo_receptor.ino

--- nodo_receptor.ino
+++ nodo_receptor.ino
@@ -2,6 +2,8 @@
 #include "Arduino.h"
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include "HT_SSD1306Wire.h"
+#include <ArduinoJson.h>
 
 // Configuración de Wi-Fi
 const char* ssidPrimaria = "Cybersys_Ext";
@@ -18,21 +20,24 @@ const char* mqttServer = "192.168.1.57";
 const int mqttPort = 1883;
 const char* mqttTopic = "lora/received";
 
+// Inicialización de display OLED
+static SSD1306Wire display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_128_64, RST_OLED);
+HardwareSerial gpsSerial(2);
 WiFiClient espClient;
 PubSubClient client(espClient);
 
 // Configuración LoRa
-#define RF_FREQUENCY                                915000000 // Hz
-#define TX_OUTPUT_POWER                             14        // dBm
-#define LORA_BANDWIDTH                              0         // [0: 125 kHz,
-#define LORA_SPREADING_FACTOR                       7         // [SF7..SF12]
-#define LORA_CODINGRATE                             1         // [1: 4/5,]
-#define LORA_PREAMBLE_LENGTH                        8         // Same for Tx and Rx
-#define LORA_SYMBOL_TIMEOUT                         0         // Symbols
-#define LORA_FIX_LENGTH_PAYLOAD_ON                  false
-#define LORA_IQ_INVERSION_ON                        false
-#define RX_TIMEOUT_VALUE                            1000
-#define BUFFER_SIZE                                 100 // Define el tamaño del payload
+#define RF_FREQUENCY 915000000   // Hz
+#define TX_OUTPUT_POWER 14       // dBm
+#define LORA_BANDWIDTH 0         // [0: 125 kHz,
+#define LORA_SPREADING_FACTOR 7  // [SF7..SF12]
+#define LORA_CODINGRATE 1        // [1: 4/5,]
+#define LORA_PREAMBLE_LENGTH 8   // Same for Tx and Rx
+#define LORA_SYMBOL_TIMEOUT 0    // Symbols
+#define LORA_FIX_LENGTH_PAYLOAD_ON false
+#define LORA_IQ_INVERSION_ON false
+#define RX_TIMEOUT_VALUE 1000
+#define BUFFER_SIZE 100  // Define el tamaño del payload
 
 char txpacket[BUFFER_SIZE];
 char rxpacket[BUFFER_SIZE];
@@ -96,23 +101,23 @@ void connectMQTT() {
 }
 
 void setup() {
-    Serial.begin(115200);
-    Mcu.begin(HELTEC_BOARD, SLOW_CLK_TPYE);
+  Serial.begin(115200);
+  Mcu.begin(HELTEC_BOARD, SLOW_CLK_TPYE);
+  iniciarDisplay();
+  txNumber = 0;
+  rssi = 0;
 
-    txNumber = 0;
-    rssi = 0;
-  
-    RadioEvents.RxDone = OnRxDone;
-    Radio.Init(&RadioEvents);
-    Radio.SetChannel(RF_FREQUENCY);
-    Radio.SetRxConfig(MODEM_LORA, LORA_BANDWIDTH, LORA_SPREADING_FACTOR,
-                               LORA_CODINGRATE, 0, LORA_PREAMBLE_LENGTH,
-                               LORA_SYMBOL_TIMEOUT, LORA_FIX_LENGTH_PAYLOAD_ON,
-                               0, true, 0, 0, LORA_IQ_INVERSION_ON, true);
+  RadioEvents.RxDone = OnRxDone;
+  Radio.Init(&RadioEvents);
+  Radio.SetChannel(RF_FREQUENCY);
+  Radio.SetRxConfig(MODEM_LORA, LORA_BANDWIDTH, LORA_SPREADING_FACTOR,
+                    LORA_CODINGRATE, 0, LORA_PREAMBLE_LENGTH,
+                    LORA_SYMBOL_TIMEOUT, LORA_FIX_LENGTH_PAYLOAD_ON,
+                    0, true, 0, 0, LORA_IQ_INVERSION_ON, true);
 
-    connectWiFi();
-    client.setServer(mqttServer, mqttPort);
-    connectMQTT();
+  connectWiFi();
+  client.setServer(mqttServer, mqttPort);
+  connectMQTT();
 }
 
 void loop() {
@@ -129,21 +134,67 @@ void loop() {
   Radio.IrqProcess();
 }
 
-void OnRxDone(uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr) {
-    rssi = rssi;
-    rxSize = size;
-    memcpy(rxpacket, payload, size);
-    rxpacket[size] = '\0';
-    Radio.Sleep();
-    Serial.printf("\r\nPaquete recibido: \"%s\" con RSSI %d, longitud %d\r\n", rxpacket, rssi, rxSize);
+void iniciarDisplay() {
+  pinMode(Vext, OUTPUT);
+  digitalWrite(Vext, LOW);  // Activa Vext
+  display.init();
+  display.clear();
+  display.display();
+  display.setContrast(255);
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setFont(ArialMT_Plain_10);  // Configura fuente única
+}
 
-    if (client.connected()) {
-        if (client.publish(mqttTopic, rxpacket)) {
-            Serial.println("Mensaje enviado al broker MQTT.");
-        } else {
-            Serial.println("Error al enviar mensaje al broker MQTT.");
-        }
+void mostrarOLED(const String& mensaje1, const String& mensaje2, const String& mensaje3, const String& mensaje4, const String& mensaje5) {
+  display.clear();
+
+  // Dibuja un borde moderno
+  display.drawRect(0, 0, 128, 64);   // Rectángulo exterior
+  display.drawLine(0, 16, 128, 16);  // Línea separadora superior
+  display.drawString(5, 2, "Receptor"); 
+
+  // Muestra la información principal
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(0, 5, mensaje1);  // Línea superior (Latitud)
+  display.drawString(0, 15, mensaje2);  
+  display.drawString(0, 25, mensaje3);  
+  display.drawString(0, 35, mensaje4);  
+  display.drawString(0, 45, mensaje5);  
+
+
+  // Muestra todo en la pantalla
+  display.display();
+}
+
+void OnRxDone(uint8_t* payload, uint16_t size, int16_t rssi, int8_t snr) {
+  JsonDocument doc;
+  deserializeJson(doc, payload);
+  const char* latitud = doc["latitud"];
+  const char* longitud = doc["longitud"];
+  const char* fecha = doc["fecha"];
+  const char* hora = doc["hora"];
+  const char* Rssi = doc["Rssi"];
+  rssi = rssi;
+  rxSize = size;
+  memcpy(rxpacket, payload, size);
+  rxpacket[size] = '\0';
+  Radio.Sleep();
+  Serial.printf("\r\nPaquete recibido: \"%s\" con RSSI %d, longitud %d\r\n", rxpacket, rssi, rxSize);
+
+  if (client.connected()) {
+    if (client.publish(mqttTopic, rxpacket)) {
+      Serial.println("Mensaje enviado al broker MQTT.");
+      String mensaje1 = "Latitud" +String(latitud);
+      String mensaje2 = "Longitud" +String(longitud);
+      String mensaje3 = "fecha" +String(fecha);
+      String mensaje4 = "hora" +String(hora);
+      String mensaje5 = "Rssi" +String(Rssi);
+
+      mostrarOLED(mensaje1, mensaje2, mensaje3, mensaje4, mensaje5);
+    } else {
+      Serial.println("Error al enviar mensaje al broker MQTT.");
     }
+  }
 
-    lora_idle = true;
+  lora_idle = true;
 }
